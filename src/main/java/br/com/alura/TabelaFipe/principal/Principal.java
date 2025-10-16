392c79b2a3a178c92c2b25c1851ca58ea76e19de
Matheus Venâncio Scomparim <scompinha@hotmail.com>
2025-10-16 16:16:47 -0300

    aula 5.4

--- src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -1,9 +1,11 @@
 package br.com.alura.TabelaFipe.principal;
 
 import br.com.alura.TabelaFipe.model.Dados;
+import br.com.alura.TabelaFipe.model.Modelos;
 import br.com.alura.TabelaFipe.service.ConsumoApi;
 import br.com.alura.TabelaFipe.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
@@ -36,6 +38,21 @@ public class Principal {
         }
 
         var json = consumoApi.obterDados(endereco);
-        Dados dados = conversor.obterDados(json, Dados.class);
+        var marcas = conversor.obterListaDados(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.print("\nDigite o código da marca desejada: ");
+        var codicoMarca = input.nextLine();
+
+        endereco += "/" + codicoMarca + "/modelos";
+        json = consumoApi.obterDados(endereco);
+
+        var modeloLista = conversor.obterDados(json, Modelos.class); //pois o Modelo já está representado como uma Lista
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
     }
 }
