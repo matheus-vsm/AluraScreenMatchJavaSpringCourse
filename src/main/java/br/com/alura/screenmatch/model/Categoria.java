4f6904234923d080a77639310988f364d12ff8b6
matheus-vsm <scompinha@hotmail.com>
2025-10-17 23:11:02 -0300

    aula 1.9

--- src/main/java/br/com/alura/screenmatch/model/Categoria.java
+++ src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -13,13 +13,14 @@ public enum Categoria {
         this.categoriaOmdb = categoriaOmdb;
     }
 
-    //metodo que converte "comedy" para "comedia"
+    // O fromString serve para converter o nome da categoria que vem da API externa como "Comedy" para o enum interno para "Comedia"
     public static Categoria fromString(String text) {
-        for (Categoria categoria : Categoria.values()) {
-            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
-                return categoria;
+        for (Categoria categoria : Categoria.values()) { // percorre todas as constantes definidas no enum Categoria
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) { // compara o texto recebido com o nome em inglês da categoria (ignorando maiúsculas/minúsculas)
+                return categoria; // se encontrar correspondência, retorna a constante do enum (ex: "Comedy" -> Categoria.COMEDIA)
             }
         }
+        // se nenhuma correspondência for encontrada, lança uma exceção informando o erro
         throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
     }
 }
