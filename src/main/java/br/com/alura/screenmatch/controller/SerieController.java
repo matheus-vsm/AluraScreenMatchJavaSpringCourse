2930ccc170e8141445794a3ef02b500c6d59db27
matheus-vsm <scompinha@hotmail.com>
2025-10-24 22:13:20 -0300

    aula 3.3

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,29 +1,20 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.model.Serie;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController // notação para definir que essa classe é um controlador rest
 public class SerieController {
     @Autowired
-    private SerieRepository serieRepositorio;
+    private SerieService servico;
 
     @GetMapping("/series")
     public List<SerieDTO> obterSeries() {
-        return serieRepositorio.findAll().stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
-    }
-
-    @GetMapping("/inicio")
-    public String inicio() {
-        return "oi";
+        return servico.obterTodasSeries();
     }
 }
---
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService { // conterá a lógica de negócios de Serie
+    @Autowired
+    private SerieRepository serieRepositorio;
+
+    public List<SerieDTO> obterTodasSeries() {
+        return serieRepositorio.findAll().stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+}
